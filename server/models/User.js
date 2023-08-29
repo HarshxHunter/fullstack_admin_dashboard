26211404e321b240f29693ceabce6e2c7926b8ea
Harsh Singh Kaira <kairaharsh793@gmail.com>
2023-08-29 13:17:14 +0530

    init commit

---
+++ server/models/User.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            requireed: true,
+            min: 2,
+            max: 100,
+        },
+        email: {
+            type: String,
+            requireed: true,
+            min: 50,
+            unique: true
+        },
+        passwors: {
+            type: String,
+            requireed: true,
+            min: 5,
+        },
+        city: String,
+        state: String,
+        country: String,
+        occupation: String,
+        phoneNumber: String,
+        transcations: Array,
+        role:{
+            type: String,
+            enum: ["user", "admin", "superadmin"],
+            default: "admin"
+        },
+    },
+    { timestamps: true}
+);
+
+const User = mongoose.model("User", UserSchema);
+export default User;
